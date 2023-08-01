5c4de31bec592b0bc9b1cd7ddf335b930ccd76d5
v_lxianggli <v_lxianggli@tencent.com>
2023-08-01 18:04:36 +0800

    登录页

---
+++ pages/Setting/Setting.vue
@@ -0,0 +1,147 @@
+<template>
+	<view class="container">
+		<view class="status_bar">
+		            <!-- 这里是状态栏 -->
+		</view>
+
+			<view class="example-body">
+				<uni-row class="demo-uni-row" :width="nvueWidth">
+					<uni-col>
+						<view class="demo-uni-col dark_deep"></view>
+					</uni-col>
+				</uni-row>
+
+				<uni-row class="demo-uni-row" :width="nvueWidth">
+					<uni-col :span="12">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+					<uni-col :span="12">
+						<view class="demo-uni-col light"></view>
+					</uni-col>
+				</uni-row>
+
+				<uni-row class="demo-uni-row" :width="nvueWidth">
+					<uni-col :span="8">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+					<uni-col :span="8">
+						<view class="demo-uni-col light"></view>
+					</uni-col>
+					<uni-col :span="8">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+				</uni-row>
+
+				<uni-row class="demo-uni-row" :width="nvueWidth">
+					<uni-col :span="6">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+					<uni-col :span="6">
+						<view class="demo-uni-col light"></view>
+					</uni-col>
+					<uni-col :span="6">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+					<uni-col :span="6">
+						<view class="demo-uni-col light"></view>
+					</uni-col>
+				</uni-row>
+
+				<uni-row class="demo-uni-row" :width="nvueWidth">
+					<uni-col :span="4">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+					<uni-col :span="4">
+						<view class="demo-uni-col light"></view>
+					</uni-col>
+					<uni-col :span="4">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+					<uni-col :span="4">
+						<view class="demo-uni-col light"></view>
+					</uni-col>
+					<uni-col :span="4">
+						<view class="demo-uni-col dark"></view>
+					</uni-col>
+					<uni-col :span="4">
+						<view class="demo-uni-col light"></view>
+					</uni-col>
+				</uni-row>
+			</view>
+
+
+	</view>
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				gutter: 0,
+				nvueWidth: 730
+			}	
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.demo-uni-row {
+		margin-bottom: 10px;
+
+		// 组件在小程序端display为inline
+		// QQ、抖音小程序文档写有 :host，但实测不生效
+		// 百度小程序没有 :host
+		/* #ifdef MP-TOUTIAO || MP-QQ || MP-BAIDU */
+		display: block;
+		/* #endif */
+	}
+
+	// 支付宝小程序没有 demo-uni-row 层级
+	// 微信小程序使用了虚拟化节点，没有 demo-uni-row 层级
+	/* #ifdef MP-ALIPAY || MP-WEIXIN */
+	::v-deep .uni-row {
+		margin-bottom: 10px;
+	}
+
+	/* #endif */
+
+	.demo-uni-col {
+		height: 36px;
+		border-radius: 5px;
+	}
+
+	.dark_deep {
+		background-color: #99a9bf;
+	}
+
+	.dark {
+		background-color: #d3dce6;
+	}
+
+	.light {
+		background-color: #e5e9f2;
+	}
+
+
+	.container{
+		height: 100vh;
+	}
+	.example-body {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		padding: 5rpx 10rpx 0;
+		overflow: hidden;
+	}
+	
+	.status_bar {
+	        height: var(--status-bar-height);
+	        width: 100%;
+	}
+</style>
+
+
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,31 @@
 <template>
 	<view class="container">
+		<view class="line1">
+		<view style="height:7vh;display:flex;align-items: center;">
+			<!-- logo与名称 -->
+				 <image style="height: 5.5vh;max-height:50px;" mode="heightFix" src="/static/monicaLogo.png" @error="imageError"></image>
+				<text style="font-size: 1.4em;font-weight:bold;margin-left: 9px;">Giao</text>
+		</view>
+		</view>
 		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+		<view class="line2" style="">
+			<text style="font-size: 1.2em;font-weight:600;text-align: left;">你的专属&nbsp;AI&nbsp;助理</text>
+		</view>
+		
+		<view class="line3">
+			
+		</view>
+		<view class="line4">
+			<text>使用以下方式继续</text>
+			<uni-row>
+				<uni-col class="btn-box" :span='11'>
+					<view class="giao-btn"></view>
+				</uni-col>
+				<uni-col class="btn-box" :span='11' :offset="2">
+					<view class="giao-btn"></view>
+				</uni-col>
+			</uni-row>
+		</view>
 	</view>
 </template>
 
@@ -15,15 +37,37 @@
 			}
 		},
 		methods: {
-
 		}
 	}
 </script>
 
 <style>
 	.container {
-		padding: 20px;
+		height: 100vh;
+		padding: 0px 20px;
 		font-size: 14px;
 		line-height: 24px;
+	}
+	.line1{
+		height:20vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+	}
+	.line2{
+		height:8vh;
+		display:flex;
+		align-items: flex-start;
+	}
+	.line3{
+		height: 45vh;
+	}
+	.giao-btn{
+		border: 1px solid #0043ff;
+		border-radius: 3vh;
+		height: 40px;
+		width: 110px;
+	}
+	.btn-box{
 	}
 </style>
